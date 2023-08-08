e37a3f097ff900ede3d566f38fd3e12afffc2c2b
kpalisoc <kpalisoc@uchicago.edu>
2023-08-08 11:09:58 -0500

    Update __init__.py

--- cdf/codes/utils/__init__.py
+++ cdf/codes/utils/__init__.py
@@ -1,16 +1,6 @@
-
-import importlib
-import pkgutil
-
-# Import all symbols from submodules
-submodule_names = ['cleaning', 'geocoding', 'vizualizing']
-
-for name in submodule_names:
-    module = importlib.import_module(f'.{name}', __name__)
-    globals().update(vars(module))
-
-
-print("utils package is being imported!")
+import cleaning
+import geocoding
+import vizualizing
 
 
 
